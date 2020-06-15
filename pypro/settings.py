b20c086749f109a4b3e488bbfc60b373bf765d6c
andremenezees <andrr96@gmail.com>
2020-06-15 18:53:40 -0300

    Configurando coleta de arquivos estaticos

--- pypro/settings.py
+++ pypro/settings.py
@@ -117,4 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Configuraçao de ambiente de desenvolvimento
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Configuraçao para uploaod de arquivos
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
