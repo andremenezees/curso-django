9de3e1a74465a15f8c02978d47fadb19e5533408
andremenezees <andrr96@gmail.com>
2020-06-16 07:06:10 -0300

    Instalando collectfast
    Close #14

--- pypro/settings.py
+++ pypro/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())  # ['andredjango.herokuapp.c
 
 # Application definition
 
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
