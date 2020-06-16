2bcf670212dbd8dd188c9238e08a6e52d16b5168
andremenezees <andrr96@gmail.com>
2020-06-16 19:06:55 -0300

    Customizando usuario do django para usar email como nome de usuario Close #17

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -30,7 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
-    #objects = UserManager()
+    # objects = UserManager()
 
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
