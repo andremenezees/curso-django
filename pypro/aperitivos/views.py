5a2a8e690fb54ef0198a07afb3edcb9b3fa999af
andremenezees <andrr96@gmail.com>
2020-06-23 20:02:18 -0300

    Passando dados em python para o django
    close #49

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video={'titulo': 'Videos de gatos', 'vimeo_id': '431606560'}
+    video = {'titulo': 'Videos de gatos', 'vimeo_id': '431606560'}
     return render(request, 'aperitivos/video.html', context={'video': video})
