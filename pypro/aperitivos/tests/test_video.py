0b7e3a0bf6b2457e649657ac24dd18f69ad20e0b
andremenezees <andrr96@gmail.com>
2020-06-24 20:13:44 -0300

    Configurar banco de dados e modificar testes para serem feitos atraves do banco de dados
    close #54

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from model_mommy import mommy
 
 from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
@@ -7,9 +8,7 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def video(db):
-    v = Video(slug='gato-tapa', titulo='Videos de gatos', vimeo_id='431606560')  # Titulo da pagina
-    v.save()
-    return v
+    return mommy.make(Video)
 
 
 @pytest.fixture
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from pypro.aperitivos.models import Video
 
-videos = [
-    Video(slug='gato-tapa', titulo='Gato do tapa', vimeo_id='431606560'),  # Titulo do video
-    Video(slug='crazy-cat', titulo='Crazy cat', vimeo_id='84369192'),
-]
-
-videos_dct = {v.slug: v for v in videos}
-
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
