4be2779bcaaeb08c07a481dd706f6f53f083261c
andremenezees <andrr96@gmail.com>
2020-06-24 19:27:02 -0300

    Configurar banco de dados e modificar testes para serem feitos atraves do banco de dados
    close #54

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -7,7 +7,7 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def video(db):
-    v = Video(slug='gato-tapa', titulo='Videos de gatos', vimeo_id='431606560') #Titulo da pagina
+    v = Video(slug='gato-tapa', titulo='Videos de gatos', vimeo_id='431606560')  # Titulo da pagina
     v.save()
     return v
 
@@ -16,12 +16,15 @@ def video(db):
 def resp(client, video):
     return client.get(reverse('aperitivos:video', args=(video.slug,)))
 
+
 @pytest.fixture
 def resp_video_nao_encontrado(client, video):
-    return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
+    return client.get(reverse('aperitivos:video', args=(video.slug + 'video_nao_existente',)))
+
 
 def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
     assert resp_video_nao_encontrado.status_code == 404
+
 
 def test_status_code(resp):
     assert resp.status_code == 200
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from pypro.aperitivos.models import Video
 
 videos = [
-    Video(slug='gato-tapa', titulo='Gato do tapa', vimeo_id='431606560'), #Titulo do video
+    Video(slug='gato-tapa', titulo='Gato do tapa', vimeo_id='431606560'),  # Titulo do video
     Video(slug='crazy-cat', titulo='Crazy cat', vimeo_id='84369192'),
 ]
 
