f64026bf03a987c09c56af37f93689305eaa6237
andremenezees <andrr96@gmail.com>
2020-06-28 22:08:29 -0300

    Criando e ordenando os modulos do site.

---
+++ pypro/modulos/facade.py
@@ -0,0 +1,10 @@
+from pypro.modulos.models import Modulo
+
+
+def listar_modulos_ordenados():
+    """
+    Lista módulos ordenados por títulos
+    :return:
+    """
+
+    return list(Modulo.objects.order_by('order').all())
