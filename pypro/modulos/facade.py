c091ea55944527cd1769bade86c9b57c1aec5d61
andremenezees <andrr96@gmail.com>
2020-07-02 19:23:35 -0300

    Criandopagina módulos onde aparecem todos os modulos e seus conteudos

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -20,3 +20,7 @@ def listar_conteudos_de_modulos_ordenados(modulo: Modulo):
 
 def encontrar_conteudo(slug):
     return Conteudo.objects.select_related('modulo').get(slug=slug)
+
+
+def listar_modulos_com_aulas():
+    return Modulo.objects.order_by('order').all().prefetch_related('conteudo_set')
